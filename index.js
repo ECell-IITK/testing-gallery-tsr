666ff709d80a469deae92fa857a316ce491b545f
ayushku634 <ayushiit634@gmail.com>
2023-06-23 22:06:47 +0530

    added sections of events and sidebar using JS

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
-const menuButton = document.getElementById('menuButton');
+const arrowContainer = document.getElementById('arrowContainer');
 const menu = document.getElementById('menu');
+// const sections = document.querySelectorAll('.event');
+// const menuItems = document.querySelectorAll('#menu li');
 
-menuButton.addEventListener('click', function() {
+arrowContainer.addEventListener('click', function() {
   menu.classList.toggle('open');
 });
+// menuItems.forEach(function(menuItem) {
+//   menuItem.addEventListener('click', function() {
+//     const target = this.getAttribute('data-target');
+//     sections.forEach(function(event) {
+//       event.classList.remove('active');
+//     });
+//     document.getElementById(target).classList.add('active');
+//     menu.classList.remove('open');
+//   });
+// });
